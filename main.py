6c6676a5f48f0197abbee7120ba901577d7a42b7
shawn_le <shawn_le@solomon-3d.com>
2022-03-08 15:54:48 +0800

    + delete all codes, rewrite main

---
+++ main.py
@@ -0,0 +1,42 @@
+'''
+    Ref: Tianzhi FCOS github
+'''
+import argparse
+import os
+
+def train():
+    pass
+
+def main():
+    parser = argparse.ArgumentParser(description="PyTorch Object Detection Training")
+    parser.add_argument(
+        "--config-file",
+        default="",
+        metavar="FILE",
+        help="path to config file",
+        type=str,
+    )
+    parser.add_argument("--local_rank", type=int, default=0)
+    parser.add_argument(
+        "--skip-test",
+        dest="skip_test",
+        help="Do not test the final model",
+        action="store_true",
+    )
+    parser.add_argument(
+        "opts",
+        help="Modify config options using the command-line",
+        default=None,
+        nargs=argparse.REMAINDER,
+    )
+
+    args = parser.parse_args()
+
+    num_gpus = int(os.environ["WORLD_SIZE"]) if "WORLD_SIZE" in os.environ else 1
+    args.distributed = num_gpus > 1
+
+
+
+if __name__ == "__main__":
+    main()
+
